67a7aa3f847ee4564e5c1fb936e3a0a67722bf26
Dan Vega <danvega@gmail.com>
2024-05-24 12:26:18 -0400

    initial commit

---
+++ src/main/java/dev/danvega/chatdv_htmx/ChatController.java
@@ -0,0 +1,35 @@
+package dev.danvega.chatdv_htmx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ChatController {
+
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("")
+    public String home() {
+        return "index";
+    }
+
+    @PostMapping("/generate")
+    public String generate(@RequestParam String message, Model model) {
+        log.info("User Message: {}", message);
+        String response = chatClient.call(message);
+        model.addAttribute("response",response);
+        model.addAttribute("message",message);
+        return "response :: response";
+    }
+
+}
---
+++ src/test/java/dev/danvega/chatdv_htmx/ChatdvHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.chatdv_htmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatdvHtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
